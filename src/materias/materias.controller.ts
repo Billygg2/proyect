167d70201a31321e9da247bf9012c5d2e48d94d2
BillyS-PV <billsalme@gmail.com>
2023-11-11 19:40:14 -0500

    e

--- src/materias/materias.controller.ts
+++ src/materias/materias.controller.ts
@@ -1,13 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { MateriasService } from './materias.service';
 
-
+// El controlador se asocia a la ruta '/materias'
 @Controller('materias')
 export class MateriasController {
 
-    constructor(private materiasService: MateriasService){}
+    constructor(private materiasService: MateriasService) {}
+
+    // Decorador get para la ruta '/materias'
     @Get()
-    getMaterias(){
+    getMaterias() {
+        // Llama al método getMaterias() del servicio MateriasService para obtener todas las materias
         return this.materiasService.getMaterias();
+    }
+
+    // Decorador get para la ruta '/materias/id'
+    @Get(':id')
+    // El decorador @Param('id') indica que el valor del parámetro 'id' se obtendrá de la ruta correspondiente
+    getMateriaById(@Param('id') id: number) {
+        // Llama al método getMateria() del servicio MateriasService para obtener una materia específica por su ID
+        return this.materiasService.getMateria(id);
     }
 }
--- src/materias/materias.service.ts
+++ src/materias/materias.service.ts
@@ -2,29 +2,34 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class MateriasService {
-    private materias =[{
-        id: 1,
-        nombre : "Lenguaje",
-        tipo : 2
-    },
-    {
-        id: 2,
-        nombre : "Matematicas",
-        tipo : 1
-    },
-    {
-        id: 3,
-        nombre : "Sociales",
-        tipo : 2
-    }
-]
+    // Declaramos un arreglo de materias como propiedad privada de la clase
+    private materias = [
+        {
+            id: 1,
+            nombre: 'Lenguaje',
+            tipo: 2,
+        },
+        {
+            id: 2,
+            nombre: 'Matematicas',
+            tipo: 1,
+        },
+        {
+            id: 3,
+            nombre: 'Sociales',
+            tipo: 2,
+        },
+    ];
 
-    getMaterias(){
+    // Este es un método para obtener todas las materias
+    getMaterias() {
         return this.materias;
     }
-    getMateria(id: string){
-        return this.materias[1];
+
+    // Creanis otro método solamente para obtener una materia por su ID
+    getMateria(id: number) {
+        // Utilizamos el método "find()"" para buscar una materia con la ID que coincida con el parámetro "id"
+        // Utilizamos el operador de igualdad débil (==) para comparar los valores del ID sin tener en cuenta el tipo de datos
+        return this.materias.find(materia => materia.id == id);
     }
-
-
 }
